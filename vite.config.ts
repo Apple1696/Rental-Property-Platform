6d9c82ca6817ec1e5b98cab9574450bff86069db
Hoang Pham <hoang.pham@affina.com.vn>
2025-06-23 09:13:39 +0700

    change prefix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig, loadEnv } from "vite"
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
   const apiTarget = env.VITE_API_URL || 'http://34.124.245.123:8080'
   
   return {
