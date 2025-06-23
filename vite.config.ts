74625099d21e526cb1e1bd7b19b022e0d547ea25
Hoang Pham <hoang.pham@affina.com.vn>
2025-06-23 09:18:50 +0700

    add log load .env

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,10 @@ import { defineConfig, loadEnv } from "vite"
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), 'VITE_')
+  console.log('✅ Loaded VITE_API_URL:', env.VITE_API_URL)
   const apiTarget = env.VITE_API_URL || 'http://34.124.245.123:8080'
-  
+  console.log('api targer: ', apiTarget)
+
   return {
     plugins: [react(), tailwindcss()],
     resolve: {
